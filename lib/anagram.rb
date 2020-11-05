e8cf80f55d9cdfd47784846052c537f4257e4fe0
cmarker <cmarker4@gmail.com>
2020-11-05 09:23:04 -0600

    commit tree 1e57a717012a3769f5432f974509d4a4834e5799

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+    attr_accessor :words
+
+    def initialize(words)
+        @words = words
+    end
+
+    def match(array)
+        arr = []
+        tmp_words = words.chars.sort
+        array.each do |word|
+            temp = word.chars.sort
+            if temp == tmp_words
+                arr << word
+            end
+        end
+        arr
+
+
+        
+    end
+end
+
